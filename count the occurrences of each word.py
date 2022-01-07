4b5b5da8ea069e029d586e9fc43b5577ee7d2697
ANU KRISHNAN AG <97270962+Anukrishnanag@users.noreply.github.com>
2022-01-07 10:07:51 +0530

    Add files via upload

---
+++ count the occurrences of each word.py
@@ -0,0 +1,13 @@
+str1=input("Enter a String:")
+counts = dict()
+words = str1.split()
+for word in words:
+    if word in counts:
+        counts[word]+=1
+    else:
+         counts[word]=1
+for k,v in counts.items():
+    print(k,v)
+    
+
+
